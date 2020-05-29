971eda338b5806f04537e84d0f0814e288bef9a8
Andrea <crescenziandrea@live.it>
2020-05-29 17:03:26 +0200

    prova2

--- app/src/main/java/it/crescenziandrea/myapplication/MainActivity.java
+++ app/src/main/java/it/crescenziandrea/myapplication/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getText(R.string.app_name);
+        getText(R.string.app_name);
     }
 }
