fd5fa367ab7dd2e94e5a7d78a90ab3daa58b46d0
Andrea Crescenzi <45431466+anCx04@users.noreply.github.com>
2020-05-29 17:17:00 +0200

    Update MainActivity.java

--- app/src/main/java/it/crescenziandrea/myapplication/MainActivity.java
+++ app/src/main/java/it/crescenziandrea/myapplication/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getText(R.string.app_name);
         getText(R.string.app_name);
+        getText(R.string.app_name);
     }
 }
